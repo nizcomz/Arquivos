c7d6995127bf419c7dba808e5aa5d8f3b0c67fb2
nizcomz <hugoniz78@gmail.com>
2025-11-19 10:02:55 -0300

    Adicionar arquivos de exemplo: estudos_1.py, estudos_2.py, estudos_3.py e banco de dados meubanco.db

---
+++ Projetos/estudos_py/estudos_1.py
@@ -0,0 +1,14 @@
+nome = "joao" # Variavel do tipo string
+idade = 25 # Variavel do tipo inteiro 
+if idade >= 18:
+    print (f"{nome} e maior de idade")
+
+# Exemplo: verificar se um número é par ou ímpar
+num = 7
+if num % 2 == 0:
+    print("Par")
+else:
+    print("Impar")
+
+
+         
---
+++ Projetos/estudos_py/estudos_2.py
@@ -0,0 +1,4 @@
+#Lista de frutas
+frutas = ["maca", "banana", "laranja"]
+for fruta in frutas:
+    print(fruta)
